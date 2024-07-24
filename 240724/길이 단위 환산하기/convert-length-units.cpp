625f5362e432c020f4d1d6cc92bf3abd101ced35
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-07-24 19:49:20 +0900

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240724/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a;
+    cout<<fixed;
+    cout.precision(1);
+    cin >> a;
+    a = a*30.48;
+    cout<<a;
+    return 0;
+}
