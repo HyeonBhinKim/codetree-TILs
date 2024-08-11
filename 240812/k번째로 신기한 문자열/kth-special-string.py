7a7bd3abaee08cbe53b8a7830cb8e36b89f96cf6
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-12 01:37:15 +0900

    Title: k번째로 신기한 문자열, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240812/k번째로 신기한 문자열/kth-special-string.py
+++ 240812/k번째로 신기한 문자열/kth-special-string.py
@@ -3,19 +3,17 @@ n, k = int(n), int(k)
 
 n_lst = []
 
-# a가 b로 시작하는지를 확인합니다.
 def starts_with(a, b):
-    # b의 길이가 더 길 수는 없습니다.
     if len(a) < len(b):
         return False
     
-    # b의 길이만큼 살펴보며, a와 문자열이 완벽히 동일한지 확인합니다.
     return a[:len(b)] == b
 
 for _ in range(n):
-    word = input()
-    if starts_with(word, T):
-        n_lst.append(word)
+    tmp = input()
+    if starts_with(tmp, T):
+        n_lst.append(tmp)
 
-n_lst.sort()    
+n_lst.sort()
+
 print(n_lst[k-1])
