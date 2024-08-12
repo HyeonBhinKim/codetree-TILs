04c8d8eece8839614eda43016bf895cacd6e3bc4
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-12 23:45:21 +0900

    Title: 국영수 순이지, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240812/국영수 순이지/korean-english-math-order.py
+++ 240812/국영수 순이지/korean-english-math-order.py
@@ -3,10 +3,11 @@ people = []
 
 for _ in range(n):
     person = list(map(str, input().split()))
+    person[1],person[2],person[3] = int(person[1]),int(person[2]),int(person[3])
     people.append(person)
 
-people.sort(key=lambda x:(x[1], x[2], x[3]), reverse=True)
+people.sort(key=lambda x:(-x[1], -x[2], -x[3]))
 
 
 for i in people:
-    print(' '.join(i))
+    print(i[0],i[1],i[2],i[3])
