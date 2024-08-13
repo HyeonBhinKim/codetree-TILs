38b83a00ea233be88722345e36c485aa91cb145f
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-14 00:06:37 +0900

    Title: 왔다 갔던 구역 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240814/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240814/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,21 +1,21 @@
 n = int(input())
-n_lst = [0]*601
-idx = 300
+n_lst = [0]*31
+idx = 0
 ans = 0
 
 for _ in range(n):
     x, d = map(str, input().split())
     if d == 'R':
         for i in range(int(x)):
-            if idx == 600:
+            if idx == 30:
                 idx = -1
-            idx += 1
             n_lst[idx] += 1
             if n_lst[idx] == 2:
                 ans += 1
+            idx += 1
     else:
         for i in range(int(x)):
-            if idx == -601:
+            if idx == -31:
                 idx = 0
             idx -= 1
             n_lst[idx] += 1
