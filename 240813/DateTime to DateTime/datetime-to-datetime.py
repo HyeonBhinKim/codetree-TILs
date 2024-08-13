325179f035d381f7e36a2b0e8b7772116aeba821
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-13 18:54:46 +0900

    Title: DateTime to DateTime, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240813/DateTime to DateTime/datetime-to-datetime.py
+++ 240813/DateTime to DateTime/datetime-to-datetime.py
@@ -1,17 +1,8 @@
-def DateTime(a,b,c):
-    total = 0
-    if a >= 11:
-        if a == 11 and b < 11:
-            return -1
-        if a == 11 and b == 11 and c < 11:
-            return -1
-        total += 24*60*(a-11) + 60*(b-11) + (c-11)
-
-        return total
-    else:
-        return -1
-
-
 a, b, c = map(int, input().split())
 
-print(DateTime(a,b,c))
+diff = (a * 24 * 60 + b * 60 + c) - (11 * 24 * 60 + 11 * 60 + 11)
+
+if diff < 0:
+    print(-1)
+else:
+    print(diff)
