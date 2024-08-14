cdde01e6addaa480ee114b23c8a9e298dae12685
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-14 18:36:32 +0900

    Title: 연속되는 수 2, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240814/연속되는 수 2/continuous-number2.py
+++ 240814/연속되는 수 2/continuous-number2.py
@@ -1,25 +1,26 @@
 N = int(input())
 
-n_lst = [0] * 1001
-targets = ''
+# 숫자를 입력받기 위한 리스트
+numbers = []
 
 for _ in range(N):
-    target = input()
-    targets += target
+    target = int(input())
+    numbers.append(target)
 
-cnt = 1
+max_count = 1  # 연속된 최대 횟수
+current_count = 1  # 현재 연속된 횟수
 
-for i in range(N):
-    if i == 0 or targets[i] == targets[i-1]:
-        if i:
-            cnt += 1
-        else:
-            pass
-    
+for i in range(1, N):
+    if numbers[i] == numbers[i - 1]:
+        current_count += 1  # 현재 숫자와 이전 숫자가 같으면 카운트 증가
     else:
-        cnt = 1
-    
-    if n_lst[i] < cnt:
-        n_lst[i] = cnt
+        # 연속이 끊기면 최대값 갱신
+        if current_count > max_count:
+            max_count = current_count
+        current_count = 1  # 카운트 초기화
 
-print(max(n_lst))
+# 루프가 끝난 후 마지막 연속 카운트 체크
+if current_count > max_count:
+    max_count = current_count
+
+print(max_count)
