082766461276c7193421c87003f41313d0a73883
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-14 15:33:33 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 251ms, Memory: 55MB, Status: Passed - CodeTree

---
+++ 240814/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
@@ -0,0 +1,23 @@
+OFFSET = 1000
+
+n_lst = [[0]*2001for _ in range(2001)]
+ans = 0
+
+for _ in range(3):
+    x1, y1, x2, y2 = map(int, input().split())
+
+    if _ < 2:
+        for i in range(x1, x2):
+            for j in range(y1, y2):
+                if n_lst[i][j] == 0:
+                    ans += 1
+                    n_lst[i][j] = 1
+
+    else:
+        for i in range(x1, x2):
+            for j in range(y1, y2):
+                if n_lst[i][j] == 1:
+                    ans -= 1
+                    n_lst[i][j] = 0
+
+print(ans)
