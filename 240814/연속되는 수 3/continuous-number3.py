6bde49cc2edb4bd335a3659a5c334f5088d312b4
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-14 19:30:54 +0900

    Title: 연속되는 수 3, Time: 159ms, Memory: 25MB, Status: Passed - CodeTree

--- 240814/연속되는 수 3/continuous-number3.py
+++ 240814/연속되는 수 3/continuous-number3.py
@@ -1,16 +1,20 @@
 N = int(input())
 
-n_lst = [input() for _ in range(N)]
+n_lst = [int(input()) for _ in range(N)]
 
-ans, cnt = 0, 0
+max_length = 1  # 최대 길이 초기화
+current_length = 1  # 현재 연속 길이 초기화
 
-for i in range(N):
-    if i > 0 and n_lst[i][0] == n_lst[i-1][0] :
-        cnt += 1
-
+for i in range(1, N):
+    # 이전 숫자와 현재 숫자의 부호가 같으면
+    if (n_lst[i] > 0 and n_lst[i - 1] > 0) or (n_lst[i] < 0 and n_lst[i - 1] < 0):
+        current_length += 1  # 연속 길이 증가
     else:
-        cnt = 1
-    
-    ans = max(ans, cnt)
+        # 부호가 다르면 현재 길이 초기화
+        max_length = max(max_length, current_length)  # 최대 길이 갱신
+        current_length = 1  # 현재 길이 초기화
 
-print(ans)
+# 마지막 연속 길이 체크
+max_length = max(max_length, current_length)
+
+print(max_length)
