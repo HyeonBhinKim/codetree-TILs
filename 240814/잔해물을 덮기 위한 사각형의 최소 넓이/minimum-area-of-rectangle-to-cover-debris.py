5aa2e3de0b48c3f37c8d9346bd148a681d74cbdd
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-14 17:16:29 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 237ms, Memory: 55MB, Status: Passed - CodeTree

--- 240814/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240814/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -1,42 +1,47 @@
-# 첫 번째 직사각형의 좌표를 입력받습니다.
-x1_1, y1_1, x2_1, y2_1 = map(int, input().split())
-# 두 번째 직사각형의 좌표를 입력받습니다.
-x1_2, y1_2, x2_2, y2_2 = map(int, input().split())
-
-# OFFSET을 적용
+MAX_R = 2001
 OFFSET = 1000
+
+n_lst = [[0]*MAX_R for _ in range(MAX_R)]
+
+minx, miny = 2002, 2002
+maxx, maxy = -1, -1
+
+x1_1, y1_1, x2_1, y2_1 = map(int, input().split())
+
 x1_1 += OFFSET
 y1_1 += OFFSET
 x2_1 += OFFSET
 y2_1 += OFFSET
+
+for i in range(x1_1, x2_1):
+    for j in range(y1_1, y2_1):
+            n_lst[i][j] = 1
+
+x1_2, y1_2, x2_2, y2_2 = map(int, input().split())
+
 x1_2 += OFFSET
 y1_2 += OFFSET
 x2_2 += OFFSET
 y2_2 += OFFSET
 
-# 남아 있는 잔해물을 덮기 위한 최소 직사각형의 좌표를 계산
-min_x = min(x1_1, x1_2)
-max_x = max(x2_1, x2_2)
-min_y = min(y1_1, y1_2)
-max_y = max(y2_1, y2_2)
+for i in range(x1_2, x2_2):
+    for j in range(y1_2, y2_2):
+        if n_lst[i][j] == 1:
+            n_lst[i][j] = 0
 
-# 최소 직사각형의 넓이를 계산
-cover_area = (max_x - min_x) * (max_y - min_y)
 
-# 남아 있는 영역이 겹치는 경우 넓이를 다시 계산
-overlap_x1 = max(x1_1, x1_2)
-overlap_y1 = max(y1_1, y1_2)
-overlap_x2 = min(x2_1, x2_2)
-overlap_y2 = min(y2_1, y2_2)
+for i in range(MAX_R):
+    for j in range(MAX_R):
+        if n_lst[i][j] == 1:
+            minx = min(minx, i)
+            miny = min(miny, j)
+            maxx = max(maxx, i)
+            maxy = max(maxy, j)
 
-# 겹치는 영역이 존재하는지 확인
-if overlap_x1 < overlap_x2 and overlap_y1 < overlap_y2:
-    # 겹치는 부분의 넓이를 계산
-    overlap_area = (overlap_x2 - overlap_x1) * (overlap_y2 - overlap_y1)
+
+if minx <= maxx and miny <= maxy:
+    ans = (maxx + 1 - minx) * (maxy + 1 - miny)
 else:
-    overlap_area = 0  # 겹치는 영역이 없으면 넓이는 0
+    ans = 0  # 남아 있는 부분이 없을 경우
 
-# 남아 있는 부분을 덮기 위한 최소 직사각형의 넓이에서 겹치는 부분 넓이를 제외
-final_area = cover_area - overlap_area
-
-print(final_area)
+print(ans)
