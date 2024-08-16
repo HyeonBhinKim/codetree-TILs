12b0b8091e1fc01cbc601b72efdb56566752a592
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-16 19:41:07 +0900

    Title: 선두를 지켜라, Time: 137ms, Memory: 40MB, Status: Wrong Answer - CodeTree

---
+++ 240816/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,41 @@
+MAX_R = 1000001
+MAX_T = 0
+ans = -1
+flg = 1
+N, M = map(int, input().split())
+
+n_lst = [0 for _ in range(MAX_R)]
+m_lst = [0 for _ in range(MAX_R)]
+
+for _ in range(N):
+    v, t = map(int, input().split())
+    if MAX_T == 0:
+        for i in range(t):
+            n_lst[i+1] = n_lst[i] + v
+    else:
+        for i in range(MAX_T, MAX_T+t):
+            n_lst[i+1] = n_lst[i] + v
+    MAX_T += t
+
+MAX_T = 0
+
+for _ in range(M):
+    v, t = map(int, input().split())
+    if MAX_T == 0:
+        for i in range(t):
+            m_lst[i+1] = m_lst[i] + v
+    else:
+        for i in range(MAX_T, MAX_T+t):
+            m_lst[i+1] = m_lst[i] + v
+    MAX_T += t
+
+
+for i in range(MAX_T):
+    if (n_lst[i] - m_lst[i]) * flg < 0:
+        flg *= -1
+        ans += 1
+
+if ans > 0:
+    print(ans)
+else:
+    print(0)
