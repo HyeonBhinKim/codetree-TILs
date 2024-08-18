b8fcd9840e1a65611a3fa79b209b07c1a684fed1
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-18 23:03:58 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240818/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240818/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,7 +1,8 @@
 N, K, P, T = map(int, input().split())
 MAX_N = N + 1
-n_lst = [0 for _ in range(MAX_N)]
-n_lst[P] = K + 1
+n_lst = [0 for _ in range(MAX_N)] # 감염목록
+n_lst[P] = 1
+hs_lst = [0 for _ in range(MAX_N)] # 악수횟수
 
 handshake = [0]*251
 
@@ -11,25 +12,25 @@ for _ in range(T):
 
 for i in handshake:
     if i:
-        if n_lst[i[0]] > 1 or n_lst[i[1]] > 1:
-            if n_lst[i[0]] > 1 and n_lst[i[1]] > 1:
-                n_lst[i[0]] -= 1
-                n_lst[i[1]] -= 1
-            elif n_lst[i[0]] > 1 and n_lst[i[1]] == 0:
-                n_lst[i[0]] -= 1
-                n_lst[i[1]] = K + 1
-            elif n_lst[i[0]] == 0 and n_lst[i[1]] > 1:
-                n_lst[i[0]] = K + 1
-                n_lst[i[1]] -= 1
-            elif n_lst[i[0]] > 1 and n_lst[i[1]]:
-                n_lst[i[0]] -= 1
-            elif n_lst[i[0]] and n_lst[i[1]] > 1:
-                n_lst[i[1]] -= 1
+        # 감염되어있으면 악수 횟수 +1
+        if n_lst[i[0]]:
+            hs_lst[i[0]] += 1
+        if n_lst[i[1]]:
+            hs_lst[i[1]] += 1
+
+        # i[0]이 감염되어있고 K번 이하로 악수했으면 i[1]을 감염
+        if n_lst[i[0]] and hs_lst[i[0]] <= K:
+            n_lst[i[1]] = 1
+
+        # i[1]이 감염되어있고 K번 이하로 악수했으면 i[0]을 감염
+        if n_lst[i[1]] and hs_lst[i[1]] <= K:
+            n_lst[i[0]] = 1
+            
         
 n_lst = n_lst[1:MAX_N+1]
 
 for i in n_lst:
-    if i >= 1:
+    if i:
         print(1, end='')
     else:
         print(0, end='')
