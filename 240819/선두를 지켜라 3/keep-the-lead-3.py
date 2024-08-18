a3512f276de442d089a2878b64325908165573c0
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-19 00:22:00 +0900

    Title: 선두를 지켜라 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240819/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,48 @@
+N, M = map(int, input().split())
+MAX_R = 21
+
+n_lst = [0] * MAX_R
+m_lst = [0] * MAX_R
+
+time = 0
+
+for _ in range(N):
+    v, t = map(int, input().split())
+    for i in range(t):
+        time += 1
+        n_lst[time] = n_lst[time-1] + v
+
+time = 0
+
+for _ in range(M):
+    v, t = map(int, input().split())
+    for i in range(t):
+        time += 1
+        m_lst[time] = m_lst[time-1] + v    
+
+ans = 0
+tmp = [0, 0]
+
+for t in range(1, time+1):
+    if n_lst[t] > m_lst[t]:
+        if tmp[0] == 1 and tmp[1] == 0:
+            pass
+        else:
+            tmp[0], tmp[1] = 1, 0
+            ans += 1
+
+    elif n_lst[t] < m_lst[t]:
+        if tmp[0] == 0 and tmp[1] == 1:
+            pass
+        else:
+            tmp[0], tmp[1] = 0, 1
+            ans += 1
+
+    else:
+        if tmp[0] == 1 and tmp[1] == 1:
+            pass
+        else:
+            tmp[0], tmp[1] = 1, 1
+            ans += 1
+
+print(ans)
