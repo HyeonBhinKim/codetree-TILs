950804e21f89a24e97fcab7f6c43f8c6bb2ec25f
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-20 22:42:32 +0900

    Title: 작은 구슬의 이동, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240820/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,30 @@
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+n, t = map(int, input().split())
+r, c, d = map(str, input().split())
+x, y = int(r), int(c)
+
+dx, dy = [1, 0, 0, -1], [0, -1, 1, 0]
+
+D = {
+    "U": 0,
+    "D": 3,
+    "R": 2,
+    "L": 1
+}
+
+dir_num = D[d]
+
+
+while t+1:
+    nx, ny = x + dx[dir_num], y + dy[dir_num]
+    if not in_range(nx, ny):  # check whether position is out of grid
+        dir_num = 3 - dir_num # change direction
+    
+    # move
+    x, y = x + dx[dir_num], y + dy[dir_num]
+
+    t -= 1
+
+print(x, y)
