0739bfce2f45723f167862a14cba1b61ec10ba4c
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-20 22:55:25 +0900

    Title: 작은 구슬의 이동, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240820/작은 구슬의 이동/small-marble-movement.py
+++ 240820/작은 구슬의 이동/small-marble-movement.py
@@ -1,29 +1,32 @@
+# 변수 선언 및 입력
+n, t = tuple(map(int, input().split()))
+x, y, c_dir = tuple(input().split())
+
+# 각 알파벳 별 방향 번호를 설정합니다.
+mapper = {
+    'R': 0,
+    'D': 1,
+    'U': 2,
+    'L': 3
+}
+x, y, move_dir = int(x) - 1, int(y) - 1, mapper[c_dir]
+
+dxs = [0, 1, -1,  0]
+dys = [1, 0,  0, -1]
+
+
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
 
-n, t = map(int, input().split())
-r, c, d = map(str, input().split())
-x, y = int(r)-1, int(c)-1 # 1~n 까지니까 0 ~ n-1까지로
 
-dx, dy = [1, 0, 0, -1], [0, -1, 1, 0]
-
-D = {
-    "U": 0,
-    "D": 3,
-    "R": 2,
-    "L": 1
-}
-
-dir_num = D[d]
-
-
-while t:
-    nx, ny = x + dx[dir_num], y + dy[dir_num]
-    if in_range(nx, ny):  # 범위 체크
+# simulation 진행
+for _ in range(t):
+    nx, ny = x + dxs[move_dir], y + dys[move_dir]
+    # 범위 안에 들어온다면 그대로 진행합니다.
+    if in_range(nx, ny):
         x, y = nx, ny
+    # 벽에 부딪힌다면, 방향을 바꿔줍니다.
     else:
-        dir_num = 3 - dir_num # 방향 전환
+        move_dir = 3 - move_dir
 
-    t -= 1
-
-print(x+1, y+1)
+print(x + 1, y + 1)
