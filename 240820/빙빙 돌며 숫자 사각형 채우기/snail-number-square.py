6933d2ce440db756eadb7eab5e8889f955cfa0c1
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-20 23:57:30 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 136ms, Memory: 25MB, Status: Passed - CodeTree

--- 240820/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
+++ 240820/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -1,8 +1,8 @@
 def in_range(x, y):
-    return 0 <= x and x < m and 0 <= y and y < n
+    return 0 <= x < m and 0 <= y < n  # m: 열 수, n: 행 수
 
-n, m  = map(int, input().split())
-answer = [[0]*m for _ in range(n)]
+n, m = map(int, input().split())
+answer = [[0] * m for _ in range(n)]
 dy, dx = [0, 1, 0, -1], [1, 0, -1, 0]
 
 y, x = 0, 0
@@ -10,7 +10,7 @@ dir_num = 0  # 0: 오른쪽, 1: 아래쪽, 2: 왼쪽, 3: 위쪽
 
 answer[y][x] = 1
 
-for i in range(2, n * n + 1):
+for i in range(2, n * m + 1):  # n * m 만큼 반복해야 함
     # 현재 방향 dir를 기준으로 그 다음 위치 값을 계산합니다.
     nx, ny = x + dx[dir_num], y + dy[dir_num]
     
@@ -18,9 +18,10 @@ for i in range(2, n * n + 1):
     # 시계방향으로 90'를 회전합니다.
     if not in_range(nx, ny) or answer[ny][nx] != 0:
         dir_num = (dir_num + 1) % 4
+        nx, ny = x + dx[dir_num], y + dy[dir_num]  # 방향 변경 후 새로운 위치 계산
 
     # 그 다음 위치로 이동한 다음 배열에 올바른 값을 채워넣습니다.
-    x, y = x + dx[dir_num], y + dy[dir_num]
+    x, y = nx, ny
     answer[y][x] = i
 
 for i in answer:
