70552dffbfd8e48778c2f0bd71f023ab3b77cc90
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-21 21:26:01 +0900

    Title: 거울에 레이저 쏘기 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240821/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240821/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -1,30 +1,31 @@
-dxs, dys = [1, 0, -1, 0], [0, -1, 0, 1] # 동, 북, 서, 남
+N = int(input())
+dxs, dys = [1, 0, -1, 0], [0, 1, 0, -1] # 동, 북, 서, 남
 
 # y축 = 행, x축 = 열
 def startpoint(N, K):
     K -= 1
         # return y, x, d 
     if K//N == 3:
-        return (N-1)-K%N, 0, 0 # 동
+        return (N-1)-K%N, 0, 0 # 동 +x
     elif K//N == 2:
-        return N-1, K%N, 1 # 북
+        return N-1, K%N, 3 # -y
     elif K//N:
-        return K%N, N-1, 2 # 서
+        return K%N, N-1, 2 # 서 -x
     else:
-        return 0, (N-1)-K%N, 3 # 남
+        return 0, (N-1)-K%N, 1 # +y
 
 def in_range(x, y):
     return 0 <= x < N and 0 <= y < N
 
-    # 동 / 북
-    # 서 / 남
-    # 남 \ 동
-    # 북 \ 서
+    # 동에서 \ 보면 북으로 보고 +y
+    # 서 \ 남 -y
+    # 남 -y 3 \ 동 +x 0 right
+    # 북 +y 1 \ 서 -x 2 right
 
-    # 동 \ 남
-    # 서 \ 북
-    # 남 / 서
-    # 북 / 동
+    # 동 / -y
+    # 서 / +y
+    # 남 / -x
+    # 북 / +x
 
 def dxy_right(x, y, d):
     d = (d+1)%4
@@ -38,25 +39,18 @@ def dxy_left(x, y, d):
 
 
 def reflect(x, y, d):
+    global N
     # print('reflect', x, y, d)
     if not in_range(x, y):
         return 0
     else:
-        if n_lst[y][x] == "/": #배열 y축 = 행, x축 = 열
-            if d == 0 or d == 2:
-                nx, ny, d = dxy_right(x, y, d)
-            else:
-                nx, ny, d = dxy_left(x, y, d)
-            return reflect(nx, ny, d) + 1
+        if n_lst[y][x] == "!": #배열 y축 = 행, x축 = 열
+            nx, ny, d = dxy_right(x, y, d)
         else:
-            if d == 1 or d == 3:
-                nx, ny, d = dxy_right(x, y, d)
-            else:
-                nx, ny, d = dxy_left(x, y, d)
-            return reflect(nx, ny, d) + 1
+            nx, ny, d = dxy_left(x, y, d)
+        return reflect(nx, ny, d) + 1
 
 
-N = int(input())
 
 n_lst = [''for _ in range(N)]
 
