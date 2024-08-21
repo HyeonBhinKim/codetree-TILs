bf3110a49d467db1bb337d6698b7dbf13ddc57d1
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-21 18:06:05 +0900

    Title: 격자 위의 편안한 상태, Time: 180ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240821/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -0,0 +1,32 @@
+N, M = map(int, input().split())
+
+def in_range(x, y):
+    return 0 <= x < N and 0 <= y < N
+
+def is_colored(x, y):
+    global N
+    colored = 0
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if in_range(nx, ny) and n_lst[nx][ny]:
+            colored += 1
+    
+    if colored > 2:
+        return True
+
+    return False
+
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+
+
+n_lst = [[0]*N for _ in range(N)]
+
+for _ in range(M):
+    r, c = map(int, input().split())
+    r, c = r-1, c-1
+    n_lst[r][c] = 1
+
+    if is_colored(r, c):
+        print(1)
+    else:
+        print(0)
