139a947832f3f7c2f1d33dc94483732a9caf671c
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-22 15:17:26 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 134ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240822/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 240822/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -6,20 +6,31 @@ dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
 n = int(input())
 
 x, y = n//2, n//2
-dirt = 0
+dirt = 3
 
 n_lst = [[0]*n for _ in range(n)]
 
 n_lst[y][x] = 1
 
+tmp = cnt = 1
+bi = 2
+
 for i in range(2, (n**2)+1):
     nx, ny = x + dx[dirt], y + dy[dirt]
+    cnt -= 1
 
-    if not in_range(nx, ny) or n_lst[ny][nx] != 0:
+    if not in_range(nx, ny) or n_lst[ny][nx] != 0 or cnt == 0:
+        bi -= 1
+        if bi:
+            cnt = tmp
+        else:
+            bi = 2
+            tmp += 1
+            cnt = tmp
         dirt = (dirt + 1) % 4
-        nx, ny = x + dx[dirt], y + dy[dirt]
 
-    x, y = nx, ny
+
+    x, y = x + dx[dirt], y + dy[dirt]
     n_lst[y][x] = i
 
 for i in n_lst:
