aaf001ade9d3423ebc868a51ae524abf9d7df635
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-23 16:23:10 +0900

    Title: 체크판위에서 2, Time: 224ms, Memory: 25MB, Status: Passed - CodeTree

--- 240823/체크판위에서 2/on-the-checkboard-2.py
+++ 240823/체크판위에서 2/on-the-checkboard-2.py
@@ -8,14 +8,14 @@ arr = [
 cnt = 0
 
 # 완전 탐색
-for i in range(R):
-    for j in range(C):
+for i in range(1, R):
+    for j in range(1, C):
         for k in range(i+1, R-1):
             for l in range(j+1, C-1):
                 # 그 중 색깔이 전부 달라지는 경우에만 개수를 세줍니다.
-                if arr[0][0] != arr[i][j] and \
-                   arr[i][j] != arr[k][l] and \
-                   arr[k][l] != arr[R-1][C-1]:
+                if (arr[0][0] != arr[i][j] and
+                   arr[i][j] != arr[k][l] and
+                   arr[k][l] != arr[R-1][C-1]):
                     cnt += 1
 
 print(cnt)
