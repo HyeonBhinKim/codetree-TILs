ae267e68ef08e2c4d2d2362c89b7c1b271bf917c
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-23 15:40:35 +0900

    Title: 최고의 13위치, Time: 204ms, Memory: 25MB, Status: Passed - CodeTree

--- 240823/최고의 13위치/best-place-of-13.py
+++ 240823/최고의 13위치/best-place-of-13.py
@@ -1,16 +1,13 @@
 N = int(input())
-n_lst = []
-
-for _ in range(N):
-    tmp = list(map(int, input().split()))
-    n_lst.append(tmp)
-
-
+n_lst = [
+    list(map(int, input().split()))
+    for _ in range(N)
+]
 
 ans = 0
 for i in range(N):
     for j in range(N-2):
         cnt = n_lst[i][j] + n_lst[i][j+1] + n_lst[i][j+2]
-    ans = max(ans, cnt)
+        ans = max(ans, cnt)
 
 print(ans)
