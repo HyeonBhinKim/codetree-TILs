a146930c409878c63cb600812ebc06b307228ae8
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-23 18:54:35 +0900

    Title: Carry 피하기 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240823/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,24 @@
+def carry(i, j, k):
+    while (i+j+k):
+        if i%10 + j%10 + k%10 > 9:
+            return False
+        i //= 10
+        j //= 10
+        k //= 10
+    return True
+
+
+
+n = int(input())
+
+n_lst = list(int(input()) for _ in range(n))
+
+ans = -1
+
+for i in range(n-2):
+    for j in range(i+1, n-1):
+        for k in range(j+1, n-2):
+            if carry(n_lst[i], n_lst[j], n_lst[k]):
+                ans = max(ans, n_lst[i]+n_lst[j]+n_lst[k])
+
+print(ans)
