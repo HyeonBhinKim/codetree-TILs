c001534788ee076ab1bd1a64732ab311b0fe68dc
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-23 15:39:10 +0900

    Title: 최고의 13위치, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240823/최고의 13위치/best-place-of-13.py
+++ 240823/최고의 13위치/best-place-of-13.py
@@ -10,7 +10,7 @@ for _ in range(N):
 ans = 0
 for i in range(N):
     for j in range(N-2):
-        cnt = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+        cnt = n_lst[i][j] + n_lst[i][j+1] + n_lst[i][j+2]
     ans = max(ans, cnt)
 
 print(ans)
