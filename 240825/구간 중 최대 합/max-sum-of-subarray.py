115530e390a1ca72c1dfa508cd6d37776680c6e4
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-25 04:40:30 +0900

    Title: 구간 중 최대 합, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240825/구간 중 최대 합/max-sum-of-subarray.py
+++ 240825/구간 중 최대 합/max-sum-of-subarray.py
@@ -3,7 +3,7 @@ n, k = map(int, input().split())
 n_lst = list(map(int, input().split()))
 
 ans = 0
-for i in range(n-k):
+for i in range(n-k+1):
     ans = max(ans, sum(n_lst[i:i+k]))
 
 print(ans)
