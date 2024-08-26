780f02c1e0e42081978a50c2bd40abe6fe55b3f8
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-26 15:20:36 +0900

    Title: G or H 3, Time: 173ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 240826/G or H 3/G-or-H-3.py
@@ -0,0 +1,19 @@
+N, K = map(int, input().split())
+
+MAX_N = 10001
+n_lst = [0] * MAX_N
+ans = 0
+
+for _ in range(N):
+    loc, spel = tuple(input().split())
+    loc = int(loc)
+    if spel == 'G':
+        n_lst[loc] = 1
+    else:
+        n_lst[loc] = 2
+
+for i in range(1, MAX_N-K-1):
+    tmp = sum(n_lst[i:i+K+1])
+    ans = max(ans, tmp)
+
+print(ans)
