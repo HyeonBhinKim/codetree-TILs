029f9d3f90c8d7b10e369a9c689616e6d836bd32
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-28 02:42:34 +0900

    Title: 밭의 높이를 고르게하기, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240828/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240828/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -5,10 +5,9 @@ n_lst = list(map(int, input().split()))
 min_val = 10001
 
 for i in range(N-T+1):
-    tmp_lst = n_lst[i:i+T]
     tmp_val = 0
-    for j in tmp_lst:
-        tmp_val += abs(j-H)
+    for j in range(i, i+T):
+        tmp_val += abs(n_lst[j]-H)
     min_val = min(min_val, tmp_val)
 
 print(min_val)
