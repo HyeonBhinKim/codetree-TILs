494d708e1fc6275277ec3c772d523a172568697f
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-28 02:13:30 +0900

    Title: G or H 2, Time: 127ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240828/G or H 2/G-or-H-2.py
@@ -0,0 +1,22 @@
+N = int(input())
+
+MAX_N = 101
+n_lst = [0] * MAX_N
+max_loc = 0
+ans = 0
+
+for _ in range(N):
+    loc, spel = tuple(input().split())
+    loc = int(loc)
+    max_loc = max(max_loc, loc)
+    if spel == 'G':
+        n_lst[loc] = 1
+    else:
+        n_lst[loc] = -1
+
+for i in range(max_loc+2):
+    for j in range(max_loc+2):
+        if ((1 or -1) in n_lst[i:j+1]) and sum(n_lst[i:j+1]) == 0:
+            ans = j - i
+
+print(ans)
