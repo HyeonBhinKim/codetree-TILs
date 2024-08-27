e22ace534804aaa23e2e31d85f3151321b96b7f4
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-28 02:18:34 +0900

    Title: G or H 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240828/G or H 2/G-or-H-2.py
+++ 240828/G or H 2/G-or-H-2.py
@@ -2,6 +2,7 @@ N = int(input())
 
 MAX_N = 101
 n_lst = [0] * MAX_N
+min_loc = 101
 max_loc = 0
 ans = 0
 
@@ -9,13 +10,14 @@ for _ in range(N):
     loc, spel = tuple(input().split())
     loc = int(loc)
     max_loc = max(max_loc, loc)
+    min_loc = min(min_loc, loc)
     if spel == 'G':
         n_lst[loc] = 1
     else:
         n_lst[loc] = -1
 
-for i in range(max_loc+2):
-    for j in range(max_loc+2):
+for i in range(min_loc, max_loc+2):
+    for j in range(min_loc, max_loc+2):
         if ((1 or -1) in n_lst[i:j+1]) and sum(n_lst[i:j+1]) == 0:
             ans = j - i
 
