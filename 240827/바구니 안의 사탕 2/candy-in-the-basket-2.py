1f02c9831ac7e0d6d6ae59278bbc1fcad6ee19b5
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-27 17:26:40 +0900

    Title: 바구니 안의 사탕 2, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240827/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240827/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -2,13 +2,20 @@ MAX_N = 401
 N, K = map(int, input().split())
 n_lst = [0]*MAX_N
 
-for i in range(N):
-    elem, loc = map(int,input().split())
-    n_lst[loc] = elem
+for _ in range(N):
+    elem, loc = map(int, input().split())
+    n_lst[loc] += elem  # 같은 위치에 여러 바구니가 가능하므로 += 사용
 
 ans = 0
 
-for j in range(1, MAX_N-2*K):
-    ans = max(ans, sum(n_lst[j:j+2*K+1]))
+cumsum = [0] * MAX_N
+for i in range(1, MAX_N):
+    cumsum[i] = cumsum[i - 1] + n_lst[i]
+
+# j의 범위를 K부터 MAX_N-K까지 설정
+for j in range(K, MAX_N - K):
+    # 구간 [j-K, j+K]의 합 계산
+    total_candies = cumsum[j + K] - cumsum[j - K - 1]
+    ans = max(ans, total_candies)
 
 print(ans)
