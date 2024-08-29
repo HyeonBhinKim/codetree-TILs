871b0282d3c3b43a024dceafcd2be5885dd97344
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-29 16:51:26 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240829/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
+++ 240829/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -9,7 +9,7 @@ for i in range(N):
 	for j in range(i+1, N):
 		tmpx = abs(n_lst[i][0] - n_lst[j][0])
 		tmpy = abs(n_lst[i][1] - n_lst[j][1])
-		tmpxy = tmpx + tmpy
+		tmpxy = ((tmpx**2) + (tmpy**2))**0.5
 		if minxy > tmpxy:
 			minxy = tmpxy
 			x, y = tmpx, tmpy
