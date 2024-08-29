2c231d2471d20c7a379ae5f044328ae3150d4320
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-29 18:35:30 +0900

    Title: 스승의 은혜 2, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 240829/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240829/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -7,16 +7,15 @@ ans = 0
 
 for i in range(N):
     n_lst[i] //= 2
-    for j in range(N):
-        cnt = 0
-        tmp = 0
-        for k in range(N):
-            cnt += 1
-            tmp += n_lst[k]
-            if tmp > B:
-                cnt -= 1
-                break
-        ans = max(ans, cnt)
+    cnt = 0
+    tmp = 0
+    for k in range(N):
+        cnt += 1
+        tmp += n_lst[k]
+        if tmp > B:
+            cnt -= 1
+            break
+    ans = max(ans, cnt)
             
     n_lst[i] *= 2
 
