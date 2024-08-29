2bb99a75aaacef24540e30e0ba002a3a152e884b
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-29 17:18:50 +0900

    Title: 삼각형 만들기, Time: 160ms, Memory: 26MB, Status: Passed - CodeTree

--- 240829/삼각형 만들기/create-triangle.py
+++ 240829/삼각형 만들기/create-triangle.py
@@ -1,35 +1,32 @@
-N = int(input())
-points = [tuple(map(int, input().split())) for _ in range(N)]
+# 변수 선언 및 입력
+n = int(input())
+points = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
 
+
+# 삼각형의 넓이에 2를 곱한 값을 반환합니다.
+def area(x1, y1, x2, y2, x3, y3):
+    return abs((x1 * y2 + x2 * y3 + x3 * y1) -
+               (x2 * y1 + x3 * y2 + x1 * y3))
+
+
+# 3개의 점을 모두 골라보면서
+# 조건을 만족하는 경우 중
+# 최대 넓이를 계산합니다.
 max_area = 0
-
-for i in range(N):
-    for j in range(N):
-        if i == j:
-            continue
-        for k in range(N):
-            if k == i or k == j:
-                continue
-            
-            # i와 j 점을 연결하여 x축에 평행한 변을 만든다.
-            if points[i][1] == points[j][1]:  # y좌표가 같음
-                base_length = abs(points[i][0] - points[j][0])  # 밑변 길이
-                height = abs(points[k][1] - points[i][1])  # 높이
-                area = base_length * height
-                max_area = max(max_area, area)
-
-            # i와 k 점을 연결하여 x축에 평행한 변을 만든다.
-            if points[i][1] == points[k][1]:  # y좌표가 같음
-                base_length = abs(points[i][0] - points[k][0])  # 밑변 길이
-                height = abs(points[j][1] - points[i][1])  # 높이
-                area = base_length * height
-                max_area = max(max_area, area)
-
-            # j와 k 점을 연결하여 x축에 평행한 변을 만든다.
-            if points[j][1] == points[k][1]:  # y좌표가 같음
-                base_length = abs(points[j][0] - points[k][0])  # 밑변 길이
-                height = abs(points[i][1] - points[j][1])  # 높이
-                area = base_length * height
-                max_area = max(max_area, area)
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            # x값이 같은 쌍이 있으며, y값 역시 같은 쌍이 있는 경우에만
+            # 최대 넓이를 계산합니다.
+            x1, y1 = points[i]
+            x2, y2 = points[j]
+            x3, y3 = points[k]
+            if (x1 == x2 or x1 == x3 or x2 == x3) and \
+                (y1 == y2 or y1 == y3 or y2 == y3):
+                max_area = max(max_area, 
+                               area(x1, y1, x2, y2, x3, y3))
 
 print(max_area)
