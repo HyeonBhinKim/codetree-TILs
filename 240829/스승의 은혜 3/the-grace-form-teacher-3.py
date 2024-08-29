f0907fbe2013f023b0f0fee664706da7d3f9c3ad
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-29 22:22:49 +0900

    Title: 스승의 은혜 3, Time: 173ms, Memory: 25MB, Status: Passed - CodeTree

--- 240829/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240829/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -4,27 +4,36 @@ n_lst = []
 
 for _ in range(N):
     p, s = map(int, input().split())
-    tot = p+s
-    n_lst.append([tot,p,s])
-    
+    total_cost = p + s
+    n_lst.append([total_cost, p, s])
+
 ans = 0
 
 for i in range(N):
-    tmp = [n_lst[j]for j in range(N)]
-    tmp[i][1] //= 2
-    tmp[i][0] = tmp[i][1]+tmp[i][2]
+    # 쿠폰을 i번째 학생의 선물에 적용
+    coupon_price = n_lst[i][1] // 2
+    coupon_total_cost = coupon_price + n_lst[i][2]
+    
+    # 나머지 학생들의 선물 가격을 포함
+    costs = []
+    for j in range(N):
+        if j == i:
+            costs.append(coupon_total_cost)  # 쿠폰 사용한 학생
+        else:
+            costs.append(n_lst[j][0])  # 쿠폰 사용하지 않은 학생
 
-    tmp.sort(key=lambda x :x[0])
-    # print(tmp)
+    # 비용 정렬
+    costs.sort()
 
     budget = 0
-    cnt = 0
+    count = 0
 
-    for k in range(N):
-        if budget + tmp[k][0] > B:
+    for cost in costs:
+        if budget + cost > B:
             break
-        budget += tmp[k][0]
-        cnt += 1
-    ans = max(ans, cnt)
+        budget += cost
+        count += 1
+
+    ans = max(ans, count)
 
 print(ans)
