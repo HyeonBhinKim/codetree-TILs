ed620ffc87d95346dda1306aff423608f1f5ae90
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-30 14:10:40 +0900

    Title: 정보에 따른 숫자 2, Time: 144ms, Memory: 25MB, Status: Passed - CodeTree

--- 240830/정보에 따른 숫자 2/number-based-on-information-2.py
+++ 240830/정보에 따른 숫자 2/number-based-on-information-2.py
@@ -1,27 +1,26 @@
 T, a, b = map(int, input().split())
 
-# S는 1, N은 2
-alpha_lst = [0]*1001
+# S와 N의 위치를 저장할 리스트
+s_positions = []
+n_positions = []
 
 for _ in range(T):
     alpha, loc = input().split()
     loc = int(loc)
-    if alpha =='S':
-        alpha_lst[loc] = 1
+    if alpha == 'S':
+        s_positions.append(loc)
     else:
-        alpha_lst[loc] = 2
+        n_positions.append(loc)
 
 cnt = 0
 
-for i in range(a, b+1):
-    nears = 1001
-    nearn = 1001
-    for j in range(1001):
-        if alpha_lst[j] == 1 and abs(i-j) < nears:
-            nears = abs(i-j)
-        elif alpha_lst[j] == 2 and abs(i-j) < nearn:
-            nearn = abs(i-j)
-    if abs(i-nears) <= abs(i-nearn):
+for i in range(a, b + 1):
+    # S와 N의 가장 가까운 거리 초기화
+    nears = min(abs(i - pos) for pos in s_positions)
+    nearn = min(abs(i - pos) for pos in n_positions)
+    
+    # 조건 확인
+    if nears <= nearn:
         cnt += 1
 
 print(cnt)
