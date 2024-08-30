f6094c7e1a126d8d40862e58a309f494b774716a
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-30 15:27:58 +0900

    Title: 흥미로운 숫자 2, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240830/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240830/흥미로운 숫자 2/interesting-numbers-2.py
@@ -1,20 +1,22 @@
 def interstednumber(n):
-    n_lst = [0]*(n+1)
-    cnt_1 = 0
-    cnt_m1 = 0
+    n_lst = {}
     n = str(n)
     for i in n:
-        n_lst[int(i)] += 1
-    
-    for num in n_lst:
-        if num == 1:
-            cnt_1 += 1
-        elif num > 1:
-            cnt_m1 += 1
-    
-    if cnt_1 == 1 and cnt_m1 == 1:
-        return True
-
+        if int(i) in n_lst:
+            n_lst[int(i)] += 1
+        else:
+            n_lst[int(i)] = 1
+    if len(n_lst) > 2:
+        return False
+    cnt1 = 0
+    cntm = 0
+    for i in n_lst:
+        if i == 1:
+            cnt1 += 1
+        else:
+            cntm += 1
+        if cnt1 == 1 and cntm == 1:
+            return True
     return False
 
 
