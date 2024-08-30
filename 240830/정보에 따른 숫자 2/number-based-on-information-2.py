c81fdbb0a3f6e04a09e32cbdf4420de34da38593
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-30 14:06:38 +0900

    Title: 정보에 따른 숫자 2, Time: 126ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240830/정보에 따른 숫자 2/number-based-on-information-2.py
+++ 240830/정보에 따른 숫자 2/number-based-on-information-2.py
@@ -1,31 +1,27 @@
 T, a, b = map(int, input().split())
 
 # S는 1, N은 2
-# alpha_lst = [0]*1001
+alpha_lst = [0]*1001
 
-s_lst = []
-n_lst = []
 for _ in range(T):
     alpha, loc = input().split()
     loc = int(loc)
-    # if a<=loc<=b and alpha =='S':
     if alpha =='S':
-        s_lst.append(loc)
+        alpha_lst[loc] = 1
     else:
-    # elif a<=loc<=b and alpha =='N':
-        n_lst.append(loc)
+        alpha_lst[loc] = 2
 
 cnt = 0
 
 for i in range(a, b+1):
-    for s in s_lst:
-        for n in n_lst:
-            if abs(s-i) <= abs(n-i):
-                cnt += 1
-    
+    nears = 1001
+    nearn = 1001
+    for j in range(1001):
+        if alpha_lst[j] == 1 and abs(i-j) < nears:
+            nears = abs(i-j)
+        elif alpha_lst[j] == 2 and abs(i-j) < nearn:
+            nearn = abs(i-j)
+    if abs(i-nears) <= abs(i-nearn):
+        cnt += 1
+
 print(cnt)
-    # for j in range(a, b+1):
-    #     if alpha_lstp[j] == 1:
-    #         s_lst.append(j)
-    #     elif alpha_lst[j] == 2:
-    #         n_lst.append(j)
