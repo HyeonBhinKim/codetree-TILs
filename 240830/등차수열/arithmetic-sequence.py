384dfc8480b7da0548d0766a352687cb8a81993d
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-30 16:25:03 +0900

    Title: 등차수열, Time: 179ms, Memory: 25MB, Status: Passed - CodeTree

--- 240830/등차수열/arithmetic-sequence.py
+++ 240830/등차수열/arithmetic-sequence.py
@@ -1,13 +1,23 @@
+MAX_A = 100
+
+# 변수 선언 및 입력
 n = int(input())
-n_lst = list(map(int, input().split()))
 
-cnt = 0
+arr = list(map(int, input().split()))
 
-for i in range(n):
-    for j in range(i+1, n):
-        ai, aj = n_lst[i], n_lst[j]
-        k = (ai + aj) / 2
-        if k == (ai + aj) // 2:
-            cnt += 1
+ans = 0
 
-print(cnt)
+# 각 숫자에 대해 
+# 등차수열의 개수를 확인합니다.
+for x in range(1, MAX_A + 1):
+	# 모든 쌍을 만들어 등차수열의 개수를 확인합니다.
+    cnt = 0
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            if arr[i] + arr[j] == 2 * x:
+                cnt += 1
+
+    ans = max(ans, cnt)
+    
+print(ans)
