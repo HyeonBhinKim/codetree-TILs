8eacdcf65fb7a1fe42da65683f0d84aaf917aca8
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-30 17:33:00 +0900

    Title: 빙산의 일각 2, Time: 113ms, Memory: 28MB, Status: Passed - CodeTree

--- 240830/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 240830/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -1,19 +1,20 @@
 N = int(input())
-
 h_lst = [int(input())for _ in range(N)]
 
 max_h = max(h_lst)
 
 cnt = 0
-
 for i in range(1, max_h):
     tmp = 0
-    for j in range(1, N):
-        if h_lst[j-1]-i > 0 and h_lst[j]-i == 0:
-           tmp += 1
-        if j == N-1 and h_lst[j]-i > 0:
-            tmp += 1
-
-    cnt = max(cnt, tmp) 
+    icing = False
+    for j in h_lst:
+        if j > i:
+            if not icing:
+                icing = True
+                tmp += 1
+        else:
+            icing = False
+    
+    cnt = max(cnt, tmp)
 
 print(cnt)
