df6a8bbefef774dee87b11071aa24e6b5bf9f5fd
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-08-30 16:10:03 +0900

    Title: 등차수열, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240830/등차수열/arithmetic-sequence.py
@@ -0,0 +1,14 @@
+n = int(input())
+n_lst = list(map(int, input().split()))
+
+n_lst.sort()
+
+cnt = 0
+
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(n_lst[i]+1,n_lst[j]):
+            if n_lst[j]-k == k-n_lst[i]:
+                cnt += 1
+
+print(cnt)
