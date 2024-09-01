c40166d0246fabf07387f2aab5d047f9cd53fa19
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-09-02 02:59:08 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240902/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -0,0 +1,18 @@
+N = int(input())
+strs = input()
+
+ans = 0
+
+for i in range(1, N-1):
+    flag = False
+    for j in range(N-i):
+        if flag:
+            break
+        for k in range(j+1, N-i+1):
+            if strs[j:j+i] == strs[k:k+i]:
+                ans += 1    
+                flag = True
+                break
+    if ans != i:
+        print(i)
+        break
