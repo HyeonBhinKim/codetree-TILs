daf6a06113cd2a3586b56ab0174317048b302c93
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-09-02 01:00:28 +0900

    Title: 좌표평면 위의 균형 2, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240902/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -0,0 +1,32 @@
+N = int(input())
+
+MAX_xy = 11
+
+points = [tuple(map(int, input().split()))for _ in range(N)]
+
+sum_diff = 101
+ans = 0
+
+for i in range(2, MAX_xy, 2): # x
+    for j in range(2, MAX_xy, 2): # y
+        fir_stage = 0
+        sec_stage = 0
+        thr_stage = 0
+        fth_stage = 0
+
+        for x, y in points:
+            if x > i and y > j:
+                fir_stage += 1
+            elif x < i and y > j:
+                sec_stage += 1
+            elif x < i and y < j:
+                thr_stage += 1
+            elif x > i and y < j:
+                fth_stage += 1
+        
+        tmp_diff = max(fir_stage, sec_stage, thr_stage, fth_stage) - min(fir_stage, sec_stage, thr_stage, fth_stage)
+        if sum_diff > tmp_diff:
+            sum_diff = tmp_diff
+            ans = max(fir_stage, sec_stage, thr_stage, fth_stage)
+
+print(ans)
