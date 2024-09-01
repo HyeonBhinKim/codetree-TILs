33c4eb71181d4d1a1ff3582dc7ccfeac738d268e
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-09-01 20:05:46 +0900

    Title: 3개의 선 2, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240901/3개의 선 2/three-lines-2.py
+++ 240901/3개의 선 2/three-lines-2.py
@@ -12,26 +12,34 @@ for i in range(3):
     dotEA = 0
     where = ''
     for j in range(11):
-        if dotEA < (len(x_lst[j]) or len(y_lst[j])):
-            if len(x_lst[j]) > len(y_lst[j]):
-                dotEA = len(x_lst[j])
-                where = 'x'
-            else:
-                dotEA = len(y_lst[j])
-                where = 'y'
+        if dotEA < len(x_lst[j]):
+            dotEA = len(x_lst[j])
+            where = 'x'
             maxline = j
 
+        if dotEA < len(y_lst[j]):
+            dotEA = len(y_lst[j])
+            where = 'y'
+            maxline = j
+
+    # print("x_lst", x_lst)
+    # print("y_lst", y_lst)
+
+
     if where == 'x':
+        # print('where안임', where)
         for k in x_lst[maxline]:
             y_lst[k].remove(maxline)
         x_lst[maxline] = []
     else:
+        # print('where안임', where)
         for k in y_lst[maxline]:
             x_lst[k].remove(maxline)
         y_lst[maxline] = []
 
 
-
+# print("x_lst", x_lst)
+# print("y_lst", y_lst)
 for l in range(11):
     if len(x_lst[l]) != 0 or len(y_lst[l]) !=0:
         print(0)
