9ab41826770af058af6289ffcdbe07a4805be2de
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-09-03 20:38:41 +0900

    Title: 독서실의 거리두기 4, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240903/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -0,0 +1,30 @@
+def max_distance_after_adding_one_seat(N, chairs):
+    occupied_positions = [i for i in range(N) if chairs[i] == '1']
+    
+    def get_min_distance(positions):
+        if len(positions) < 2:
+            return N-1
+        min_dist = N-1
+        for i in range(1, len(positions)):
+            min_dist = min(min_dist, positions[i] - positions[i - 1])
+        return min_dist
+    
+    max_min_distance = 0
+    
+    # 모든 빈 좌석 2개에 대해 새로운 사람을 추가해봅니다.
+    for i in range(N-1):
+        for j in range(i+1, N):
+            if chairs[i] == '0' and chairs[j] == '0':
+                new_positions = occupied_positions + [i] + [j]
+                new_positions.sort()
+                min_distance = get_min_distance(new_positions)
+                max_min_distance = max(max_min_distance, min_distance)
+    
+    return max_min_distance
+
+# 입력 받기
+N = int(input())
+chairs = input()
+
+# 결과 출력
+print(max_distance_after_adding_one_seat(N, chairs))
