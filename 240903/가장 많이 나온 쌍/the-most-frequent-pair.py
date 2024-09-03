66a789ef24bddcc20b817c9bb7112f81824b7e12
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-09-03 15:47:09 +0900

    Title: 가장 많이 나온 쌍, Time: 156ms, Memory: 25MB, Status: Passed - CodeTree

--- 240903/가장 많이 나온 쌍/the-most-frequent-pair.py
+++ 240903/가장 많이 나온 쌍/the-most-frequent-pair.py
@@ -1,20 +1,17 @@
 n, m = map(int, input().split())
-ab_lst = []
-for  _ in range(m):
-    a, b = map(int, input().split())
-    if a > b:
-        a, b = b, a
-    ab_lst.append([a,b])
-    
+ab_lst = [list(map(int, input().split()))for _ in range(m)]
+
+
 ans = 0
 
 for i in range(1, n+1):
     for j in range(1, n+1):
         comb = 0
         for a, b in ab_lst:
+            if a > b:
+                a, b  = b, a
             if i == a and j == b:
                 comb += 1
         ans = max(ans, comb)
-
 
 print(ans)
