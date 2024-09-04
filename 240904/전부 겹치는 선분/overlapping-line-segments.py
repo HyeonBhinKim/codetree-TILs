13f4b672ae035002383b0754454706c57d3c8f93
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-09-04 17:56:05 +0900

    Title: 전부 겹치는 선분, Time: 107ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240904/전부 겹치는 선분/overlapping-line-segments.py
@@ -0,0 +1,12 @@
+n = int(input())
+n_lst = [0] * 101
+
+for _ in range(n):
+    x1, x2 = map(int, input().split())
+    for i in range(x1, x2+1):
+        n_lst[i] += 1
+
+if max(n_lst) == n:
+    print("Yes")
+else:
+    print("No")
