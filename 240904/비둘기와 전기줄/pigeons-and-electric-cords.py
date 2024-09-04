811d542a50b144e6780e03c1a4a0098b4b9c9739
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-09-04 23:21:16 +0900

    Title: 비둘기와 전기줄, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240904/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -0,0 +1,16 @@
+N = int(input())
+pigeons = [0]*(N+1)
+moved = [False]*(N+1)
+
+ans = 0
+for _ in range(N):
+    pigeon, loc = map(int, input().split())
+    if moved[pigeon] == False:
+        moved[pigeon] = True
+        pigeons[pigeon] = loc
+    else:
+        if pigeons[pigeon] != loc:
+            ans += 1
+            pigeons[pigeon] = loc
+
+print(ans)
