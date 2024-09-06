c0c5dc9cc4714dbd427f9b18cd4bac3490550791
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-09-06 14:23:37 +0900

    Title: 순위 경쟁2, Time: 155ms, Memory: 24MB, Status: Passed - CodeTree

--- 240906/순위 경쟁2/ranking-competition2.py
+++ 240906/순위 경쟁2/ranking-competition2.py
@@ -2,6 +2,8 @@ n = int(input())
 
 rank = 0 # 시작시 0, A 1, AB 2, B 3 
 
+change = False
+
 A = 0
 B = 0
 
@@ -16,12 +18,14 @@ for _ in range(n):
         B += score
     
     if A > B and rank != 1:
+        change = True
         ans += 1
         rank = 1
-    elif A == B and rank != 2:
+    elif A == B and rank != 2 and change:
         ans += 1
         rank = 2
     elif A < B and rank != 3:
+        change = True
         ans += 1
         rank = 3
     
