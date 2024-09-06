312a54770745b07dc43369ebd6132f565229b643
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-09-06 16:12:20 +0900

    Title: 순위 경쟁, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240906/순위 경쟁/ranking-competition.py
+++ 240906/순위 경쟁/ranking-competition.py
@@ -1,28 +1,45 @@
+def rank(a, b, c): # a 1, ab 2, b 3, bc 4, c 5, ac 6, abc 7
+    return_val = 0
+    maxval = max([score1, score2, score3]) 
+    
+    # 다음과 같이 하면 상태들을 서로 겹치지 않고 정리할 수 있습니다.
+    # 1. A가 명예의 전당에 올라가 있는 경우 상태에 1을 더합니다.
+    if score1 == maxval:
+        return_val += 1
+
+    # 2. B가 명예의 전당에 올라가 있는 경우 상태에 2를 더합니다.
+    if score2 == maxval:
+        return_val += 2
+    
+    # 3. C가 명예의 전당에 올라가 있는 경우 상태에 4를 더합니다.
+    if score3 == maxval:
+        return_val += 4
+    
+    return return_val
+
+    
+
+
 n = int(input())
-    
-# 학생 점수 초기화
-scores = {'A': 0, 'B': 0, 'C': 0}
 
-# 초기 명예의 전당 상태
-hall_of_fame = set(['A', 'B', 'C'])  # 처음에는 모두 명예의 전당에 올라감
-change_count = 0
+A, B, C = 0, 0, 0
 
+ans = 0
 for _ in range(n):
-    c, s = input().split()
-    s = int(s)
-    
-    # 점수 업데이트
-    scores[c] += s
-    
-    # 현재 최고 점수 찾기
-    max_score = max(scores.values())
-    
-    # 현재 명예의 전당 업데이트
-    current_hall_of_fame = {name for name, score in scores.items() if score == max_score}
-    
-    # 조합 변화 체크
-    if current_hall_of_fame != hall_of_fame:
-        change_count += 1
-        hall_of_fame = current_hall_of_fame  # 명예의 전당 업데이트
+    p, score = input().split()
+    score = int(score)
 
-print(change_count)
+    if p == 'A':
+        if rank(A,B,C) != rank(A+score,B,C):
+            ans += 1
+        A += score
+    elif p == 'B':
+        if rank(A,B,C) != rank(A,B+score,C):
+            ans += 1
+        B += score
+    else:
+        if rank(A,B,C) != rank(A,B,C+score):
+            ans += 1
+        C += score
+
+print(ans)
