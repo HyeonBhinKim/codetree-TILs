bbfb367cc498996c7b5f5bc95497261e269d59c2
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-09-06 16:16:37 +0900

    Title: 순위 경쟁, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240906/순위 경쟁/ranking-competition.py
+++ 240906/순위 경쟁/ranking-competition.py
@@ -1,18 +1,21 @@
 def rank(a, b, c): # a 1, ab 2, b 3, bc 4, c 5, ac 6, abc 7
-    if a > c and a > b:
-        return 1
-    if a > c and a == b:
-        return 2
-    if b > a and b > c: 
-        return 3
-    if b > a and b == c:
-        return 4
-    if c > b and c > a:
-        return 5
-    if c > b and c == a:
-        return 6
-    if a == b == c:
-        return 7
+    if a > c:
+        if a > b:
+            return 1
+        elif a == b:
+            return 2
+    elif b > a:
+        if b > c: 
+            return 3
+        elif b == c:
+            return 4
+    elif c > b:
+        if c > a:
+            return 5
+        elif c == a:
+            return 6
+    elif a == b == c:
+        return 
 
     
 n = int(input())
