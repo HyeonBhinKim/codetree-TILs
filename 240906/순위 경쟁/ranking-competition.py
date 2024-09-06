0a1e41c91c9b72b06485eba2800e2c821450aa6e
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-09-06 16:13:01 +0900

    Title: 순위 경쟁, Time: 232ms, Memory: 24MB, Status: Passed - CodeTree

--- 240906/순위 경쟁/ranking-competition.py
+++ 240906/순위 경쟁/ranking-competition.py
@@ -1,18 +1,18 @@
 def rank(a, b, c): # a 1, ab 2, b 3, bc 4, c 5, ac 6, abc 7
     return_val = 0
-    maxval = max([score1, score2, score3]) 
+    maxval = max([a, b, c]) 
     
     # 다음과 같이 하면 상태들을 서로 겹치지 않고 정리할 수 있습니다.
     # 1. A가 명예의 전당에 올라가 있는 경우 상태에 1을 더합니다.
-    if score1 == maxval:
+    if a == maxval:
         return_val += 1
 
     # 2. B가 명예의 전당에 올라가 있는 경우 상태에 2를 더합니다.
-    if score2 == maxval:
+    if b == maxval:
         return_val += 2
     
     # 3. C가 명예의 전당에 올라가 있는 경우 상태에 4를 더합니다.
-    if score3 == maxval:
+    if c == maxval:
         return_val += 4
     
     return return_val
