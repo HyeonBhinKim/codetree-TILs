91ae7b1976fd7cd900cc3a98a87f88c5409f7f28
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-09-06 17:37:11 +0900

    Title: 세 수의 최대 곱, Time: 222ms, Memory: 32MB, Status: Passed - CodeTree

--- 240906/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 240906/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -12,6 +12,6 @@ if n_lst[-3] > 0 and n_lst[-2] > 0 and n_lst[-1] > 0:
 if n_lst[0] < 0 and n_lst[1] <0 and n_lst[-1] > 0:
     ans = max(ans, n_lst[0]*n_lst[1]*n_lst[-1])
 
-ans = max(ans, abs_lst[0]*abs_lst[1]*abs_lst[2])
+ans = max(ans, abs_lst[0]*abs_lst[1]*abs_lst[2]) # 어차피 음수밖에 안나오는데, 0이 있으면 그게 최댓값
 
 print(ans)
