d2803df0e17450efc2599ebd2c99ab6f09f0e57b
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-09-06 15:34:36 +0900

    Title: L, R 그리고 B, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240906/L, R 그리고 B/l-r-and-b.py
+++ 240906/L, R 그리고 B/l-r-and-b.py
@@ -13,8 +13,7 @@ for i in range(10):
         elif arr[i][j] == 'B':
             B = [i, j]
 
-
-if L[0] == R[0] == B[0] or L[1] == R[1] == B[1]:
+if L[0] > R[0] > B[0] or L[1] > R[1] > B[1] or L[0] < R[0] < B[0] or L[1] > R[1] < B[1]:
     print(abs(L[0]-B[0])+abs(L[1]-B[1])+1)
 else:
     print(abs(L[0]-B[0])+abs(L[1]-B[1])-1)
