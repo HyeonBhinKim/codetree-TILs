87cb53f2b9af818d8d38f7efeb541468d9669253
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-09-06 16:08:11 +0900

    Title: 순위 경쟁, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240906/순위 경쟁/ranking-competition.py
+++ 240906/순위 경쟁/ranking-competition.py
@@ -1,45 +1,28 @@
-def rank(a, b, c): # a 1, ab 2, b 3, bc 4, c 5, ac 6, abc 7
-    if a > c:
-        if a > b:
-            return 1
-        if a == b:
-            return 2
-    elif b > a:
-        if b > c: 
-            return 3
-        if b == c:
-            return 4
-    elif c > b:
-        if c > a:
-            return 5
-        if c == a:
-            return 6
-    elif a == b == c:
-        return 7
-
-    
-
-
 n = int(input())
+    
+# 학생 점수 초기화
+scores = {'A': 0, 'B': 0, 'C': 0}
 
-A, B, C = 0, 0, 0
+# 초기 명예의 전당 상태
+hall_of_fame = set(['A', 'B', 'C'])  # 처음에는 모두 명예의 전당에 올라감
+change_count = 0
 
-ans = 0
 for _ in range(n):
-    p, score = input().split()
-    score = int(score)
+    c, s = input().split()
+    s = int(s)
+    
+    # 점수 업데이트
+    scores[c] += s
+    
+    # 현재 최고 점수 찾기
+    max_score = max(scores.values())
+    
+    # 현재 명예의 전당 업데이트
+    current_hall_of_fame = {name for name, score in scores.items() if score == max_score}
+    
+    # 조합 변화 체크
+    if current_hall_of_fame != hall_of_fame:
+        change_count += 1
+        hall_of_fame = current_hall_of_fame  # 명예의 전당 업데이트
 
-    if p == 'A':
-        if rank(A,B,C) != rank(A+score,B,C):
-            ans += 1
-        A += score
-    elif p == 'B':
-        if rank(A,B,C) != rank(A,B+score,C):
-            ans += 1
-        B += score
-    else:
-        if rank(A,B,C) != rank(A,B,C+score):
-            ans += 1
-        C += score
-
-print(ans)
+print(change_count)
