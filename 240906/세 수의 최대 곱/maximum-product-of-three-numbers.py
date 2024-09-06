ae1bd9d0e33a3fd1bd8ed50005c898c387e54ed4
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-09-06 17:02:15 +0900

    Title: 세 수의 최대 곱, Time: 5051ms, Memory: 31MB, Status: Time Limit Exceed - CodeTree

--- 240906/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 240906/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -1,23 +1,14 @@
 import sys
+from itertools import combinations
+
 
 n = int(input())
 n_lst = list(map(int, input().split()))
 
 ans = -sys.maxsize
 
-for i in range(n-2):
-    for j in range(i+1, n-1):
-        cnt = 0
-        for k in range(j+1, n):
-            if n_lst[i] < 0:
-                cnt +=1
-            if n_lst[j] < 0:
-                cnt +=1
-            if n_lst[k] < 0:
-                cnt +=1
-            if not cnt%2:
-                tmp = n_lst[i]*n_lst[j]*n_lst[k]
-                ans = max(ans, tmp)
-
+for ns in combinations(n_lst,3):
+    i, j, k = ns
+    ans = max(ans, i*j*k)
 
 print(ans)
