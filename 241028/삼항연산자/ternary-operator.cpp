33dffb457fddc85c10c8c4de8ae832ae7a106166
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-10-28 15:48:47 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241028/삼항연산자/ternary-operator.cpp
+++ 241028/삼항연산자/ternary-operator.cpp
@@ -4,8 +4,7 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
-    cin>>n;
-    string d = n==100 ? "pass":"failure";
-    cout<<d;
+    cin>>n; 
+    cout<< (n==100 ? "pass":"failure");
     return 0;
 }
