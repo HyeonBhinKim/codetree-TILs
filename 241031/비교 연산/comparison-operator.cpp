d803ca65a2fe9095c68c452c486bc016c87043e0
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-10-31 21:56:30 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241031/비교 연산/comparison-operator.cpp
+++ 241031/비교 연산/comparison-operator.cpp
@@ -5,29 +5,11 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a,b;
     cin>>a>>b;
-    if (a<b)
-        cout<<0<<endl;
-    else
-        cout<<1<<endl;
-    if (a>b)
-        cout<<1<<endl;
-    else
-        cout<<0<<endl;
-    if (a>b)
-        cout<<0<<endl;
-    else
-        cout<<1<<endl;
-    if (a<b)
-        cout<<1<<endl;
-    else
-        cout<<0<<endl;
-    if (a!=b)
-        cout<<0<<endl;
-    else
-        cout<<1<<endl;
-    if (a==b)
-        cout<<0<<endl;
-    else
-        cout<<1<<endl;
+    cout << (a >= b) << endl;
+	cout << (a > b) << endl;
+	cout << (a <= b) << endl;
+	cout << (a < b) << endl;
+	cout << (a == b) << endl;
+	cout << (a != b) << endl;
     return 0;
 }
