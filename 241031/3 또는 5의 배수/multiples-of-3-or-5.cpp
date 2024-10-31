a76afd1f006bb9f97a9f8ab378054e775e6b9bbf
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-10-31 21:49:57 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241031/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin>>a;
+    if (a%3)
+        cout<<"NO"<<endl;
+    else
+        cout<<"YES"<<endl;
+    if (a%5)
+        cout<<"NO";
+    else
+        cout<<"YES";        
+    return 0;
+}
