fe099bc1365b5d2949071b33b201f706a68a41d7
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-10-31 21:20:40 +0900

    Title: 출석 부르기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241031/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int A;
+    cin>>A;
+    if (A==1) {
+        cout<<"John";
+    }
+    else if (A==2) {
+        cout<<"Tom";
+    }
+    else if (A==3) {
+        cout<<"Paul";
+    }
+    else{
+        cout<<"Vacancy";
+    }
+    return 0;
+}
